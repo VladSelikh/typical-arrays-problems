e5c52f0deb4245b5eac16d09dbdd2c3f2b1c0ab7
VladSelikh <vladikh2405@gmail.com>
2020-10-22 11:55:01 +0300

    fix: proceed empty array

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 
 exports.min = function min (array) {
-  if (array === undefined || array === [])
+  if (!Array.isArray(array) || array.length === 0)
   return 0;
   else return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  if (array === undefined || array === [])
+  if (array === undefined || array.length === 0)
   return 0;
   else return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  if (array === undefined || array === [])
+  if (array === undefined || array.length === 0)
   return 0;
   else return Math.avg(...array);
 }
