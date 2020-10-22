870ad371ae4d55ddeebaf4efc3f817e4c8b0f7bb
VladSelikh <vladikh2405@gmail.com>
2020-10-22 11:33:28 +0300

    feat: add three functions

--- src/index.js
+++ src/index.js
@@ -6,9 +6,13 @@ exports.min = function min (array) {
 }
 
 exports.max = function max (array) {
+  if (array === undefined || array === [])
   return 0;
+  else return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
+  if (array === undefined || array === [])
   return 0;
+  else return Math.avg(...array);
 }
